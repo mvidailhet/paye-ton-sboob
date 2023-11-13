263413ecdf22eecc931e5757adea3d40b10e7d00
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-11-13 16:44:00 +0100

    Réimportation des modules dans le sous-module admin

--- apps/front/src/app/modules/admin/admin.module.ts
+++ apps/front/src/app/modules/admin/admin.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PromotionsComponent } from './pages/promotions/promotions.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -9,6 +11,8 @@ import { AdminRoutingModule } from './admin-routing.module';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    TranslateModule,
     AdminRoutingModule
   ]
 })
--- apps/front/src/app/modules/admin/pages/promotions/promotions.component.ts
+++ apps/front/src/app/modules/admin/pages/promotions/promotions.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-promotions',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./promotions.component.scss']
 })
 export class PromotionsComponent {
+  loginForm = this.fb.group({
+    email: [''],
+    password: [''],
+  });
 
+  constructor(
+    private fb: FormBuilder,
+  ) {}
 }
